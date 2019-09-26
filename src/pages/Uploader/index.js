bbf663ff2b29596945a1bf92925b9b36b438c511
sobucki <sobucki@gmail.com>
2019-09-26 10:44:58 -0400

    Configuração do dropzone

--- src/pages/Uploader/index.js
+++ src/pages/Uploader/index.js
@@ -1,16 +1,63 @@
 import React, { Component } from 'react';
+import { faPenNib } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { Container, Field, Button } from './styles';
+import Dropzone from 'react-dropzone';
+
+import {
+ Container, Header, Field, Button 
+} from './styles';
 
 export default class Uploader extends Component {
-  state = {};
+  state = { invalidFile: false, selectedFile: null };
+
+  onDrop = (accepted, rejected) => {
+    if (Object.keys(rejected).length !== 0) {
+      this.setState({ invalidFile: true });
+    } else {
+      this.setState({ invalidFile: false, selectedFile: accepted });
+
+      console.log(accepted[0].preview);
+
+      const blobPromise = new Promise((resolve, reject) => {
+        const reader = new window.FileReader();
+        reader.readAsDataURL(accepted[0]);
+        reader.onloadend = () => {
+          const base64data = reader.result;
+          resolve(base64data);
+        };
+      });
+      blobPromise.then((value) => {
+        console.log(value);
+      });
+    }
+  };
 
   render() {
+    const { invalidFile } = this.state;
     return (
       <Container>
-        <Field>
-          <span>Arraste ou clique aqui</span>
-        </Field>
+        <Header>
+          <FontAwesomeIcon icon={faPenNib} size="lg" />
+          <span>Assinador de PDF</span>
+        </Header>
+
+        <Dropzone
+          onDrop={(accepted, rejected) => this.onDrop(accepted, rejected)}
+          accept="application/pdf"
+        >
+          {({ getRootProps, getInputProps }) => (
+            <Field {...getRootProps()}>
+              <input {...getInputProps()} />
+              {invalidFile ? (
+                <span>Arquivo inválido, favor selecionar um .pdf</span>
+              ) : (
+                <span>Arraste um arquivo PDF ou clique aqui</span>
+              )}
+            </Field>
+          )}
+        </Dropzone>
+
         <Button>Assinar</Button>
       </Container>
     );
--- src/pages/Uploader/styles.js
+++ src/pages/Uploader/styles.js
@@ -11,6 +11,23 @@ export const Container = styled.div`
   flex-direction: column;
 `;
 
+export const Header = styled.div`
+  height: 50px;
+  padding-bottom: 10px;
+  color: #b34e7e;
+
+  FontAwesomeIcon {
+    font-size: lg;
+  }
+
+  span {
+    font-size: 25px;
+    font-weight: bold;
+    text-transform: uppercase;
+    margin-left: 10px;
+  }
+`;
+
 export const Field = styled.div`
   display: flex;
   width: 80%;
